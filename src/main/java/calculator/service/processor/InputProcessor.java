95c81377813cef5f4514bcf514d05df8ac2df61c
koreaioi <koreaioi@gachon.ac.kr>
2025-10-19 17:10:25 +0900

    feat(InputProcessor): InputProcessor 생성

---
+++ src/main/java/calculator/service/processor/InputProcessor.java
@@ -0,0 +1,11 @@
+package calculator.service.processor;
+
+public class InputProcessor {
+
+    private final DelimiterManager delimiterManager;
+
+    public InputProcessor(DelimiterManager delimiterManager) {
+        this.delimiterManager = delimiterManager;
+    }
+
+}
