19a2a84866cc14eaeb4bfd5e04cf5476d3294306
koreaioi <koreaioi@gachon.ac.kr>
2025-10-19 17:11:41 +0900

    feat(StringSplitter): StringSplitter 생성

---
+++ src/main/java/calculator/service/processor/StringSplitter.java
@@ -0,0 +1,13 @@
+package calculator.service.processor;
+
+import calculator.domain.delimiter.DelimiterStorage;
+
+public class StringSplitter {
+
+    private final DelimiterStorage delimiterStorage;
+
+    public StringSplitter(DelimiterStorage delimiterStorage) {
+        this.delimiterStorage = delimiterStorage;
+    }
+
+}
