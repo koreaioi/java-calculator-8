aca2b63a013a1da09e972220df233d3c9a686542
koreaioi <koreaioi@gachon.ac.kr>
2025-10-19 17:06:05 +0900

    feat(DelimiterParser): 커스텀 구분자 추출 메서드 구현

--- src/main/java/calculator/service/processor/DelimiterParser.java
+++ src/main/java/calculator/service/processor/DelimiterParser.java
@@ -1,4 +1,14 @@
 package calculator.service.processor;
 
+import calculator.domain.delimiter.Delimiter;
+
+import static calculator.constants.DelimiterConstants.CUSTOM_DELIMITER_INDEX;
+
 public class DelimiterParser {
+
+    public Delimiter extractDelimiter(String input) {
+        char symbol = input.charAt(CUSTOM_DELIMITER_INDEX);
+        return Delimiter.from(symbol);
+    }
+
 }
