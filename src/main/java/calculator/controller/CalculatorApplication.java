68ba5dde2e02f7f65f9edc1398987be9e6451370
koreaioi <koreaioi@gachon.ac.kr>
2025-10-19 17:27:26 +0900

    feat(CalculatorApplication): 어플리케이션 실행 계획 메서드 구현

--- src/main/java/calculator/controller/CalculatorApplication.java
+++ src/main/java/calculator/controller/CalculatorApplication.java
@@ -1,8 +1,11 @@
 package calculator.controller;
 
+import calculator.domain.number.Number;
 import calculator.service.calculator.Calculator;
 import calculator.service.processor.InputProcessor;
 import calculator.view.ApplicationView;
+
+import java.util.List;
 
 public class CalculatorApplication {
 
@@ -14,6 +17,12 @@ public class CalculatorApplication {
         this.view = view;
         this.inputProcessor = inputProcessor;
         this.calculator = calculator;
+    }
+
+    public Number execute(String input) {
+        String preprocessedInput = inputProcessor.preprocessInput(input);
+        List<Number> allOperand = inputProcessor.splitToOperands(preprocessedInput);
+        return calculator.calculate(allOperand);
     }
 
 }
