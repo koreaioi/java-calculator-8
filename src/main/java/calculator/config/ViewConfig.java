093abf2268485d43e035421b00bc52b79fa9226f
koreaioi <koreaioi@gachon.ac.kr>
2025-10-19 16:43:04 +0900

    feat(ViewConfig): ViewConfig 추가

---
+++ src/main/java/calculator/config/ViewConfig.java
@@ -0,0 +1,31 @@
+package calculator.config;
+
+import calculator.view.ApplicationView;
+import calculator.view.InputReader;
+import calculator.view.OutputWriter;
+
+public class ViewConfig {
+
+    private static final ViewConfig INSTANCE = new ViewConfig();
+
+    private ViewConfig() {
+
+    }
+
+    public static ViewConfig getInstance() {
+        return INSTANCE;
+    }
+
+    public ApplicationView applicationView() {
+        return new ApplicationView(inputreader(), outputwriter());
+    }
+
+    private InputReader inputreader() {
+        return new InputReader();
+    }
+
+    private OutputWriter outputwriter() {
+        return new OutputWriter();
+    }
+
+}
