2ef901f7b1c9a4f9ab534bc8f2bc42c8a1cbe6ce
koreaioi <koreaioi@gachon.ac.kr>
2025-10-19 15:48:39 +0900

    feat(Number): 문자열에서 양의정수 변환 시, 검증 로직 추가

--- src/main/java/calculator/domain/number/Number.java
+++ src/main/java/calculator/domain/number/Number.java
@@ -16,21 +16,24 @@ public class Number {
     }
 
     public static Number from(String number) {
-        // TODO 문자열 -> 숫자 검증 로직 추가
-        return from(Integer.parseInt(number));
+        try{
+            return number.isEmpty() ? new Number(0) : new Number(Integer.parseInt(number)); // TODO 매직 넘버 상수화
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("양의 정수(1 ~ 2,147,483,647)를 입력해주세요."); // TODO 매직 리터럴 상수화
+        }
     }
 
     public void operate(Number other, IntBinaryOperator operator) {
         try{
             this.value = operator.applyAsInt(this.value, other.value);
         }catch(ArithmeticException e){
-            throw new IllegalArgumentException("양의 정수(1 ~ 2,147,483,647)를 입력해주세요."); // TODO 매직 리터럴 상수화 고려
+            throw new IllegalArgumentException("양의 정수(1 ~ 2,147,483,647)를 입력해주세요."); // TODO 매직 리터럴 상수화
         }
     }
 
     public void validateNumber(int value) {
-        if (value < 0) { // TODO 매직 넘버 상수화 고려
-            throw new IllegalArgumentException("음수는 입력할 수 없습니다."); // TODO 매직 리터럴 상수화 고려
+        if (value < 0) { // TODO 매직 넘버 상수화
+            throw new IllegalArgumentException("음수는 입력할 수 없습니다."); // TODO 매직 리터럴 상수화
         }
     }
 
