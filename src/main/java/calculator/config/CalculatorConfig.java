78316bbcc276ee8950c24b9cdc35f5ac2b5e19ca
koreaioi <koreaioi@gachon.ac.kr>
2025-10-19 16:06:15 +0900

    feat(CalculatorConfig): CalculatorConfig 추가

---
+++ src/main/java/calculator/config/CalculatorConfig.java
@@ -0,0 +1,27 @@
+package calculator.config;
+
+import calculator.service.calculator.Adder;
+import calculator.service.calculator.Calculator;
+import calculator.service.calculator.Operator;
+
+public class CalculatorConfig {
+
+    private static final CalculatorConfig INSTANCE = new CalculatorConfig();
+
+    private CalculatorConfig() {
+
+    }
+
+    public static CalculatorConfig getInstance() {
+        return INSTANCE;
+    }
+
+    public Calculator calculator() {
+        return new Calculator(operator());
+    }
+
+    public Operator operator() {
+        return new Adder();
+    }
+
+}
