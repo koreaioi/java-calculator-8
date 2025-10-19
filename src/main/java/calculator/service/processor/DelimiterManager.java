493f5126ba17dfa66d3f0f68c3572030c1729ec8
koreaioi <koreaioi@gachon.ac.kr>
2025-10-19 17:08:06 +0900

    feat(DelimiterManager): DelimiterManager 생성

---
+++ src/main/java/calculator/service/processor/DelimiterManager.java
@@ -0,0 +1,17 @@
+package calculator.service.processor;
+
+import calculator.domain.delimiter.DelimiterStorage;
+
+public class DelimiterManager{
+
+    private final DelimiterInspector inspector;
+    private final DelimiterParser parser;
+    private final DelimiterStorage storage;
+
+    public DelimiterManager(DelimiterInspector inspector, DelimiterParser parser, DelimiterStorage storage) {
+        this.inspector = inspector;
+        this.parser = parser;
+        this.storage = storage;
+    }
+
+}
