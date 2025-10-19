4e4eb72a0a927bb0c666422d99e5b0d2d7f37485
koreaioi <koreaioi@gachon.ac.kr>
2025-10-19 17:16:21 +0900

    feat(InputProcessor): 문자열을 분리하여 피연산자로 전달하는 메서드 구현

--- src/main/java/calculator/service/processor/InputProcessor.java
+++ src/main/java/calculator/service/processor/InputProcessor.java
@@ -1,11 +1,16 @@
 package calculator.service.processor;
 
+import java.util.List;
+import calculator.domain.number.Number;
+
 public class InputProcessor {
 
     private final DelimiterManager delimiterManager;
+    private final StringSplitter splitter;
 
-    public InputProcessor(DelimiterManager delimiterManager) {
+    public InputProcessor(DelimiterManager delimiterManager, StringSplitter splitter) {
         this.delimiterManager = delimiterManager;
+        this.splitter = splitter;
     }
 
     public String preprocessInput(String input) {
@@ -13,6 +18,10 @@ public class InputProcessor {
             return delimiterManager.parseDelimiter(input);
         }
         return input;
+    }
+
+    public List<Number> splitToOperands(String preprocessedInput) {
+        return splitter.splitToNumbers(preprocessedInput);
     }
 
 }
