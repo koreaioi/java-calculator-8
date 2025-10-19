1dc8752f1b4903c5ca71cffead35687d58e224c0
koreaioi <koreaioi@gachon.ac.kr>
2025-10-19 16:57:20 +0900

    feat(DelimiterInspector): DelimiterInspector 생성

---
+++ src/main/java/calculator/service/processor/DelimiterInspector.java
@@ -0,0 +1,17 @@
+package calculator.service.processor;
+
+public class DelimiterInspector {
+
+    /* TODO
+    * DelimiterInspector 는 커스텀 구분자가 있는 지 검사한다. (검사 = 확인 + 검증)
+    * 1. 커스텀 구분자 입력 형식이 존재하는 지 확인한다.
+    * 2. 있다면 return true
+    * 3. 없다면 커스텀 구분자 입력 형식을 잘못 작성했는 지 검증한다. (검증 내역은 기능 구현 목록을 참고)
+    * 4. 검증까지 통과한다면 return false (커스텀 구분자를 입력하지 않겠다는 사용자의 의도로 파악)
+    * */
+
+    public boolean inspectDelimiter(String delimiter) {
+        return false;
+    }
+
+}
