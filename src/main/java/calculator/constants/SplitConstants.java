5bf507bccadc4c208f4f4977e0b55a81a059a91e
koreaioi <koreaioi@gachon.ac.kr>
2025-10-19 17:13:36 +0900

    feat: 문자열 분리패턴 생성 메서드 구현

---
+++ src/main/java/calculator/constants/SplitConstants.java
@@ -0,0 +1,9 @@
+package calculator.constants;
+
+public class SplitConstants {
+
+    public static final String SPLIT_PATTERN_PREFIX = "[";
+    public static final String SPLIT_PATTERN_SUFFIX = "]";
+    public static final String EMPTY_STRING = "";
+
+}
--- src/main/java/calculator/service/processor/StringSplitter.java
+++ src/main/java/calculator/service/processor/StringSplitter.java
@@ -1,6 +1,11 @@
 package calculator.service.processor;
 
+import calculator.domain.delimiter.Delimiter;
 import calculator.domain.delimiter.DelimiterStorage;
+
+import java.util.stream.Collectors;
+
+import static calculator.constants.SplitConstants.*;
 
 public class StringSplitter {
 
@@ -8,6 +13,13 @@ public class StringSplitter {
 
     public StringSplitter(DelimiterStorage delimiterStorage) {
         this.delimiterStorage = delimiterStorage;
+    }
+
+    public String createSplitPattern() {
+        return delimiterStorage.getAllDelimiter()
+                .stream()
+                .map(Delimiter::toString)
+                .collect(Collectors.joining(EMPTY_STRING, SPLIT_PATTERN_PREFIX, SPLIT_PATTERN_SUFFIX));
     }
 
 }
