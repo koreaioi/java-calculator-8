21b5d694153589a64cb134d895db4ed667da6ba7
koreaioi <koreaioi@gachon.ac.kr>
2025-10-19 16:39:57 +0900

    feat(ApplicationView): ApplicationView 생성, 입력 요청 안내 메세지 구현

---
+++ src/main/java/calculator/view/ApplicationView.java
@@ -0,0 +1,18 @@
+package calculator.view;
+
+public class ApplicationView {
+
+    private final InputReader inputReader;
+    private final OutputWriter outputWriter;
+
+    public ApplicationView(InputReader inputReader, OutputWriter outputWriter) {
+        this.inputReader = inputReader;
+        this.outputWriter = outputWriter;
+    }
+
+    public String requestInput() {
+        outputWriter.requestInput();
+        return inputReader.getInput();
+    }
+
+}
