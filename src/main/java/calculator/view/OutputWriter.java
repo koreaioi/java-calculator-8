69505f18ff83236a99bec9ea83e795c650e0aa0b
koreaioi <koreaioi@gachon.ac.kr>
2025-10-19 15:00:42 +0900

    feat(OutputWriter): 연산결과 출력 메서드 구현

--- src/main/java/calculator/view/OutputWriter.java
+++ src/main/java/calculator/view/OutputWriter.java
@@ -1,11 +1,16 @@
 package calculator.view;
 
 import static calculator.constants.ViewMessage.REQUEST_INPUT_MESSAGE;
+import static calculator.constants.ViewMessage.RESULT_FORMAT;
 
 public class OutputWriter {
 
     public void requestInput() {
         System.out.println(REQUEST_INPUT_MESSAGE.getMessage());
+    }
+
+    public void printResult(String result) {
+        System.out.println(RESULT_FORMAT.getMessage(result));
     }
 
 }
