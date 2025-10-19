012ff3705ed92fe01ed0bbccca399488bace106d
koreaioi <koreaioi@gachon.ac.kr>
2025-10-19 17:31:31 +0900

    feat(ApplicationConfig): ApplicationConfig 생성

---
+++ src/main/java/calculator/config/ApplicationConfig.java
@@ -0,0 +1,37 @@
+package calculator.config;
+
+import calculator.controller.CalculatorApplication;
+
+public class ApplicationConfig {
+
+    private static final ApplicationConfig INSTANCE = new ApplicationConfig();
+
+    private ApplicationConfig() {
+
+    }
+
+    public static ApplicationConfig getInstance() {
+        return INSTANCE;
+    }
+
+    public CalculatorApplication calculatorApplication() {
+        return new CalculatorApplication(
+                viewConfig().applicationView(),
+                processorConfig().inputPreprocessor(),
+                calculatorConfig().calculator()
+        );
+    }
+
+    private ViewConfig viewConfig() {
+        return ViewConfig.getInstance();
+    }
+
+    private ProcessorConfig processorConfig() {
+        return ProcessorConfig.getInstance();
+    }
+
+    private CalculatorConfig calculatorConfig() {
+        return CalculatorConfig.getInstance();
+    }
+
+}
