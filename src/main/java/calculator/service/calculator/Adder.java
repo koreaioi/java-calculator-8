8eee664ee4aac198997b424c9731e444677befd9
koreaioi <koreaioi@gachon.ac.kr>
2025-10-19 15:24:29 +0900

    feat(Adder): Adder 생성, 누적값 초기화 추상 메서드 구현

---
+++ src/main/java/calculator/service/calculator/Adder.java
@@ -0,0 +1,17 @@
+package calculator.service.calculator;
+
+import calculator.domain.number.Number;
+
+public class Adder implements Operator{
+
+    @Override
+    public Number getInitialAccumulator() {
+        return Number.from(0); // TODO 매직넘버 수정
+    }
+
+    @Override
+    public void operate(Number accumulator, Number operand) {
+
+    }
+
+}
