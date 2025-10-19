db517119a40dac1f9da508a00cf34ddab619b1a7
koreaioi <koreaioi@gachon.ac.kr>
2025-10-19 14:59:08 +0900

    refactor(ViewMessage): 출력 메세지 포맷 형식으로 수정

--- src/main/java/calculator/constants/ViewMessage.java
+++ src/main/java/calculator/constants/ViewMessage.java
@@ -3,7 +3,7 @@ package calculator.constants;
 public enum ViewMessage {
 
     REQUEST_INPUT_MESSAGE("덧셈할 문자열을 입력해 주세요."),
-    RESULT_FORMAT("결과 : ");
+    RESULT_FORMAT("결과 : %s");
 
     private final String message;
 
@@ -13,6 +13,13 @@ public enum ViewMessage {
 
     public String getMessage() {
         return this.message;
+    }
+
+    public String getMessage(String result) {
+        if(this.equals(RESULT_FORMAT)){
+            return String.format(this.message, result);
+        }
+        return this.getMessage();
     }
 
 }
