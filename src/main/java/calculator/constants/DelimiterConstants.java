1eefc95cccaf09dd09e456f15b8adba7ecfc01e4
koreaioi <koreaioi@gachon.ac.kr>
2025-10-19 16:51:15 +0900

    feat(DelimiterStorage): DelimiterStorage 생성

--- src/main/java/calculator/constants/DelimiterConstants.java
+++ src/main/java/calculator/constants/DelimiterConstants.java
@@ -5,4 +5,7 @@ public class DelimiterConstants {
     public static final Integer ASCII_ZERO = 48;
     public static final Integer ASCII_NINE = 57;
 
+    public static final Character COMMA = ',';
+    public static final Character COLON = ':';
+
 }
---
+++ src/main/java/calculator/domain/delimiter/DelimiterStorage.java
@@ -0,0 +1,25 @@
+package calculator.domain.delimiter;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import static calculator.constants.DelimiterConstants.COLON;
+import static calculator.constants.DelimiterConstants.COMMA;
+
+public class DelimiterStorage {
+
+    private static final DelimiterStorage INSTANCE = new DelimiterStorage();
+
+    private final Set<Delimiter> delimiters = new HashSet<>(
+            Set.of(Delimiter.from(COMMA), Delimiter.from(COLON))
+    );
+
+    private DelimiterStorage(){
+    }
+
+    public static DelimiterStorage getInstance(){
+        return INSTANCE;
+    }
+
+}
